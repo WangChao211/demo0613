7d39c3f3d83b6652f40a17dc33a3cfea77c23b55
WangChao211 <565276012@qq.com>
2018-06-13 17:16:59 +0800

    commit

---
+++ src/main/java/cn/springboot/SpringBootDemo.java
@@ -0,0 +1,9 @@
+package cn.springboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringBootDemo {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo.class, args);
+	}
+}
